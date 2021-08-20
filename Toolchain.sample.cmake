fe6e6b6319615f2e4236551fbae90898706412cb
AliSoAm <ali.sorour.amini@gmail.com>
2021-08-20 14:54:35 +0430

    Separate the toolchain file

---
+++ Toolchain.sample.cmake
@@ -0,0 +1,5 @@
+set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
+set(CMAKE_C_COMPILER arm-none-eabi-gcc)
+set(CMAKE_AR arm-none-eabi-ar)
+set(CMAKE_LINKER arm-none-eabi-gcc)
+set(CMAKE_OBJCOPY arm-none-eabi-objcopy)
